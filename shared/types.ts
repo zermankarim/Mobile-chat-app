0a26fa547e3e980a6d8430df410db7f2a8664c41
Karim Zerman <100965013+zermankarim@users.noreply.github.com>
2024-07-20 15:32:10 +0300

    Remove unused type

--- shared/types.ts
+++ shared/types.ts
@@ -84,37 +84,3 @@ export interface IButtonDrawer {
   icon: React.ReactNode | null;
   onPress: () => void;
 }
-
-export interface IPalette {
-  dark: {
-    100: string;
-    200: string;
-    300: string;
-    400: string;
-    500: string;
-    600: string;
-    700: string;
-    800: string;
-    900: string;
-    1000: string;
-  };
-  light: {
-    100: string;
-    200: string;
-    300: string;
-    400: string;
-    500: string;
-    600: string;
-    700: string;
-    800: string;
-    900: string;
-    1000: string;
-  };
-  blue: {
-    100: string;
-    200: string;
-    300: string;
-    400: string;
-    500: string;
-  };
-}
