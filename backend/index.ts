339e6b61eb14aef827ec9da3dbfd2465c7f28302
Karim Zerman <100965013+zermankarim@users.noreply.github.com>
2024-07-21 21:32:30 +0300

    Create basic structure for connecting to DB and listen app

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,42 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const mongoose = require("mongoose");
+const express = require("express");
+var cors = require("cors");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+require("dotenv").config();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const { User } = require("./models");
+
+const { PORT, MONGO_URL, CORS_ORIGIN_URL_WITH_PORT } = process.env;
+
+var corsOptions = {
+  origin: CORS_ORIGIN_URL_WITH_PORT,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
+const start = () => {
+  try {
+    mongoose.connect(MONGO_URL).then(() => {
+      console.log("Server connected to MongoDB.");
+    });
+    app.listen(PORT, () => {
+      console.log(`Server listen on port ${PORT}`);
+    });
+  } catch (e) {
+    console.error(
+      "Error during starting server or connect to MongoDB: ",
+      e.message
+    );
+  }
+};
+
+start();
+
+// Get routes
+app.get("/some", async (req, res) => {
+  const users = await User.find({});
+  console.log(users);
+  res.send("Hello World!");
+});
