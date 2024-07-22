2f1634c574568df0c3253697e8c9cef3ee619009
Karim Zerman <100965013+zermankarim@users.noreply.github.com>
2024-07-22 18:13:52 +0300

    Update models.ts

--- backend/models.ts
+++ backend/models.ts
@@ -1,21 +1,27 @@
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema({
-  uid: String,
-  firstName: String,
-  lastName: String,
-  dateOfBirth: String,
-  email: String,
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  dateOfBirth: { type: String },
+  email: { type: String, required: true },
   avatars: [String],
   friends: [String],
   backgroundColors: [String],
+  password: { type: String, required: true },
 });
 
 const chatSchema = new Schema({
-  createdAt: String,
-  createdBy: String,
-  messages: [],
-  participants: [userSchema],
+  createdAt: { type: String, required: true },
+  createdBy: { type: Schema.Types.ObjectId, ref: "User" },
+  messages: [
+    {
+      createdAt: { type: String, required: true },
+      text: { type: String },
+      sender: { type: Schema.Types.ObjectId, ref: "User" },
+    },
+  ],
+  participants: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
 
 const User = mongoose.model("users", userSchema);
