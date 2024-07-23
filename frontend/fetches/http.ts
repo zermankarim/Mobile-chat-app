37e9ad34b39b36b4592348346817764057a79c47
Karim Zerman <100965013+zermankarim@users.noreply.github.com>
2024-07-23 22:23:06 +0300

    Implement new function for get array of docs

--- frontend/fetches/http.ts
+++ frontend/fetches/http.ts
@@ -1,27 +1,6 @@
 import axios from "axios";
 import { SERVER_PORT_MAIN, SERVER_URL_MAIN } from "../config";
-import { IGetDocData, IAuthData } from "../shared/types";
-
-export const getSome = async () => {
-  try {
-    const data = await axios.get(`${SERVER_URL_MAIN}:${SERVER_PORT_MAIN}/some`);
-    console.log(data);
-    return "";
-  } catch (e: any) {
-    console.error(e.message);
-  }
-};
-
-// export const getDoc = async (
-//   collectionName: "users" | "chats",
-//   conditions: ({...params}) => {params}
-// ): Promise<IUserState | IChatDB> => {
-//   const data: IUserState | IChatDB = await axios.get(
-//     `${SERVER_URL_MAIN}:${SERVER_PORT_MAIN}/getDoc`,
-//     { params: { collectionName, field, condition, value } }
-//   );
-//   return data;
-// };
+import { IGetDocData, IAuthData, IGetDocsData } from "../shared/types";
 
 export const getDoc = async (
   collectionName: "users" | "chats",
@@ -33,12 +12,32 @@ export const getDoc = async (
 ): Promise<IGetDocData> => {
   try {
     const { data } = await axios.get(
-      `${SERVER_URL_MAIN}:${SERVER_PORT_MAIN}/getDoc`,
+      `${SERVER_URL_MAIN}:${SERVER_PORT_MAIN}/getData/getDoc`,
       { params: { collectionName, condition } }
     );
     return data;
   } catch (e: any) {
     throw new Error("Error at getDoc: ", e.message);
+  }
+};
+
+export const getDocs = async (
+  collectionName: "users" | "chats",
+  condition: {
+    field: string;
+    conditionType: "==";
+    value: string | Array<string>;
+  },
+  populateArr?: string[]
+): Promise<IGetDocsData> => {
+  try {
+    const { data } = await axios.get(
+      `${SERVER_URL_MAIN}:${SERVER_PORT_MAIN}/getData/getDocs`,
+      { params: { collectionName, condition, populateArr } }
+    );
+    return data;
+  } catch (e: any) {
+    throw new Error("Error at getDocs: ", e.message);
   }
 };
 
