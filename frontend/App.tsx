8a4f0fa082f127f2b93d9404b78110fab641a91e
Karim Zerman <100965013+zermankarim@users.noreply.github.com>
2024-07-27 17:17:11 +0300

    Add status bar

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,21 +1,17 @@
-import { useDispatch, useSelector } from "react-redux";
-import { RootState, store } from "./core/store/store";
+import { store } from "./core/store/store";
 import { FC, useCallback, useEffect, useState } from "react";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
 import * as SplashScreen from "expo-splash-screen";
 import * as Font from "expo-font";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Text } from "react-native-paper";
 import RootNavigator from "./RootNavigator";
 import { Provider as StoreProvider } from "react-redux";
 import { GlobalContext } from "./core/context/Context";
-import { connectToSocket } from "./shared/functions";
 import { Socket } from "socket.io-client";
 import { ISocketEmitEvent, ISocketOnEvent, IUserState } from "./shared/types";
-import { setMessages } from "./core/reducers/messages";
+import { StatusBar } from "react-native";
+import { theme } from "./shared/theme";
 
 const App: FC = () => {
   // States
@@ -79,6 +75,7 @@ const App: FC = () => {
       >
         <NavigationContainer>
           <SafeAreaProvider onLayout={onLayoutRootView}>
+            <StatusBar backgroundColor={theme.colors.main[400]}></StatusBar>
             <RootNavigator></RootNavigator>
           </SafeAreaProvider>
         </NavigationContainer>
