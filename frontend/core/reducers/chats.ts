66cf33976e0aef7bbfb094eecdee65256fee3c0a
Karim Zerman <100965013+zermankarim@users.noreply.github.com>
2024-08-05 11:49:54 +0300

    Update chats.ts

--- frontend/core/reducers/chats.ts
+++ frontend/core/reducers/chats.ts
@@ -1,16 +1,16 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { IChat, IChatPopulated } from "../../shared/types";
+import { IChatPopulated } from "../../shared/types";
 
 const initialState: IChatPopulated[] = [];
 
 const chatsSlice = createSlice({
-  name: "chats",
-  initialState,
-  reducers: {
-    setChats: (state, action: PayloadAction<IChatPopulated[]>) => {
-      state.splice(0, state.length, ...action.payload);
-    },
-  },
+	name: "chats",
+	initialState,
+	reducers: {
+		setChats: (state, action: PayloadAction<IChatPopulated[]>) => {
+			state.splice(0, state.length, ...action.payload);
+		},
+	},
 });
 
 export const { setChats } = chatsSlice.actions;
