8dc978bc061b1af3fce1a9ed5fa24bf88303eeb8
Karim Zerman <100965013+zermankarim@users.noreply.github.com>
2024-08-06 15:07:53 +0300

    Update currentChat.ts

--- frontend/core/reducers/currentChat.ts
+++ frontend/core/reducers/currentChat.ts
@@ -1,4 +1,4 @@
-import { IChatPopulated, IChat } from "./../../shared/types";
+import { IChatPopulated } from "./../../shared/types";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const initialState: IChatPopulated = {
